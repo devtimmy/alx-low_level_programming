299b8b253cf88f11c7b4c022f10c41e6f2756494
Fatoye Timileyin <phartoyetimileyin@gmail.com>
2022-09-09 09:11:27 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 success
  */
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
