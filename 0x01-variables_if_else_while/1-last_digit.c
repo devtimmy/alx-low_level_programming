dfd43dc4ef250ed7d130dc1ccfbb57fe64d6df02
Fatoye Timileyin <phartoyetimileyin@gmail.com>
2022-09-09 14:09:53 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,11 +12,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if ((n % 10) > 5)
-		printf("%d and is greater than 5\n", n);
-	else if (((n % 10) < 6 && ((n % 10) != 0)))
-		printf("%d and is less than 6 and not 0\n", n);
+	if ((n % 15) > 5)
+		printf("%d and is greater than 5\n", n % 15);
+	else if (((n % 15) < 6 && ((n % 15) != 0)))
+		printf("%d and is less than 6 and not 0\n", n % 15);
 	else
-		printf("%d and is 0\n", n);
+		printf("%d and is 0\n", n % 15);
 	return (0);
 }
