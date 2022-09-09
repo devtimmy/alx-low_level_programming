1bb2d0b5f6373a5feff85ee51f4093cc82278a80
Fatoye Timileyin <phartoyetimileyin@gmail.com>
2022-09-09 12:41:00 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,14 @@
 
 int main(void)
 {
-	int n;
+	int n
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n == 0)
+	if (n > 0)
 	{
-		printf("%i is zero\n", n);
+		printf("%i is positive\n", n);
 	}
 
 	else if (n < 0)
@@ -26,7 +26,7 @@ int main(void)
 
 	else
 	{
-		printf("%i is positive\n", n);
+		printf("%i is zero\n", n);
 	}
 	return (0);
 }
