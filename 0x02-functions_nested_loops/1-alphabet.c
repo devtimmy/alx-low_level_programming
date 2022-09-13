37ac26cdb5e6df5f055488dac4be3c354f95a09a
Fatoye Timileyin <phartoyetimileyin@gmail.com>
2022-09-13 11:06:23 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - A function that Prints the alphabet in lowercase
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar("\n");
+
+	return (0);
+}
