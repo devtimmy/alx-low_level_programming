17cb7acd728c6bfc7b55a7bdfc88dd73d654ed1e
Fatoye Timileyin <phartoyetimileyin@gmail.com>
2022-09-15 11:08:38 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints number
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+}
