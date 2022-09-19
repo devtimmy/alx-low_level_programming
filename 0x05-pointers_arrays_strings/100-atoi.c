62985f43d9136db36cb0e5f04f507712530d93f7
Fatoye Timileyin <phartoyetimileyin@gmail.com>
2022-09-19 16:11:01 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _atoi - convert to an int
+ * @s: string
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int i, j, n, x;
+
+	i = n = 0;
+	x = 1;
+	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+			x *= -1;
+		i++
+	}
+	j = 1;
+	while ((s[j] >= '0') && (s[j] <= '9'))
+	{
+		n = (n * 10) + x * ((s[j]) - '0');
+		j++;
+	}
+	return (n);
+}
+
+
