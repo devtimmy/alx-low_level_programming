11e079e03dc37412938f78c11e556fc3cc178573
Fatoye Timileyin <phartoyetimileyin@gmail.com>
2022-09-26 14:37:01 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,37 @@
 #include "main.h"
 /**
- * _strchr - Locates a character in a string
- * @s: String
- * @c: Character to search
- * * Return: Pointer to the first occurrence of the character c in the string s
+ *  _strspn - Locates a character in a string
+ *  @s: This is the main C string to be scanned.
+ *  @accept: This is the string containing the list of characters to match in s
+ *  Return: return count
  */
-unsigned int _strspn(char *s, char c)
+unsigned int _strspn(char *s, char *accept)
 {
-	while (*s != '\0')
+	int i, j;
+	int count = 0;
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
+
+	i = 0;
+	while (str1[i] != '\0')
 	{
-		if (*s == c)
+		j = 0;
+		while (str2[j] != '\0')
 		{
-			return (s);
+			if (str2[j] == str1[i])
+			{
+				count++;
+				break;
+			}
+			j++;
 		}
-		++s;
+		if (s[i] != accept[j])
+		{
+			break;
+		}
+		i++;
 	}
-
-	if (*s == c)
-	{
-		return (s);
-	}
-
-	return (0);
+	return (count);
 }
