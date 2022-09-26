faf99c848717b0a9722e88ddf67a13791b090d25
Fatoye Timileyin <phartoyetimileyin@gmail.com>
2022-09-26 13:21:29 -0700

    0-memset,c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int n;
 
-	/*Delacring FOR*/
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b; /*add 1 position s*/
-	} /*END FOR*/
+		s[i] = b;
+	}
 
 	return (s);
 }
