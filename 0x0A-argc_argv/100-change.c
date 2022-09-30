2ce3677e9ebc0360179d7a8085b734d33203994f
Fatoye Timileyin <phartoyetimileyin@gmail.com>
2022-09-30 12:41:39 -0700

    100-change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,6 +15,14 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
+		printf("Error\n");
+		return (1);
+	}
+
+	total = atoi(argv[1]);
+
+	if (total <= 0)
+	{
 		printf("0\n");
 		return (0);
 	}
