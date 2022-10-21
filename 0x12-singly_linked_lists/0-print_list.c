2f54cc74afba5d90fd4239f40c9b1f713b64c91f
Fatoye Timileyin <phartoyetimileyin@gmail.com>
2022-10-21 16:33:47 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,9 +8,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t lst_node;
+	size_t l_node;
 
-	lst_node = 0;
+	l_node = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		lst_node++;
+		l_node++;
 	}
-	return (lst_node);
+	return (l_node);
 }
