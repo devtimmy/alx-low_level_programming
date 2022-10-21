daf74bf75273f754a5231a9e1abda28280b4b25f
Fatoye Timileyin <phartoyetimileyin@gmail.com>
2022-10-21 16:31:05 -0700

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints list.
+ * @h: param list
+ * Return: struct
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t lst_node;
+
+	lst_node = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		lst_node++;
+	}
+	return (lst_node);
+}
